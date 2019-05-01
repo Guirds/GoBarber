df5898feb4b0aa8e2bc4067f61112d8b8138b3ab
Guilherm Santos <gui07015@gmail.com>
2019-05-01 07:00:32 -0300

    Adicionado cryptografia de senha

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -2,22 +2,26 @@ const bcrypt = require('bcryptjs')
 
 module.exports = (sequelize, DataTypes) => {
   // sequelize faz a comunicação com o banco de dados e
-  const User = sequelize.define('User', {
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    avatar: DataTypes.STRING,
-    password: DataTypes.VIRTUAl, //existe somente na aplicação e não no bd
-    password_hash: DataTypes.STRING,
-    provider: DataTypes.BOOLEAN
-  }, {
-    hooks: {
-      beforeSave: async => {
-        if(User.password){
-          User.password_hash = await bcrypt.hash(user.password, 8)//numero representa a quantidade de rounds que o bcrypt vai rodar deve ficar entre 8 e 10
+  const User = sequelize.define(
+    'User',
+    {
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      avatar: DataTypes.STRING,
+      password: DataTypes.VIRTUAL, // existe somente na aplicação e não no bd
+      password_hash: DataTypes.STRING,
+      provider: DataTypes.BOOLEAN
+    },
+    {
+      hooks: {
+        beforeSave: async User => {
+          if (User.password) {
+            User.password_hash = await bcrypt.hash(User.password, 8) // numero representa a quantidade de rounds que o bcrypt vai rodar deve ficar entre 8 e 10
+          }
         }
       }
-    },
-  })
+    }
+  )
 
   return User
 }
