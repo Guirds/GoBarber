9df581e25fe3fe72b14664cae1c7de5760d3fa43
Guilherm Santos <gui07015@gmail.com>
2019-05-01 07:01:07 -0300

    Ajustando configurações para comunicação com BD

--- src/app/models/index.js
+++ src/app/models/index.js
@@ -4,16 +4,15 @@ const fs = require('fs')
 const path = require('path')
 const Sequelize = require('sequelize')
 const basename = path.basename(__filename)
-const config = require('../../config/database')[env]
+const config = require('../../config/database')
 const db = {}
 
 const sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  )
-}
+  config.database,
+  config.username,
+  config.password,
+  config
+)
 
 fs.readdirSync(__dirname)
   .filter(file => {
