8e8354e360cbd427fc08ff671ee7e29d1352b4c7
Guilherm Santos <gui07015@gmail.com>
2019-05-03 10:14:23 -0300

    require new extention

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const session = require('express-session')
-const LokiStore = require('connect-loki')(session)
+const FileStore = require('session-file-store')(session)
 const nunjucks = require('nunjucks')
 const path = require('path')
+const flash = require('connect-flash')
 
 class App {
   constructor () {
@@ -16,12 +17,13 @@ class App {
 
   middlewares () {
     this.express.use(express.urlencoded({ extended: false }))
+    this.express.use(flash())
     this.express.use(
       session({
         name: 'root',
         secret: 'MyAppScret',
         resave: true,
-        store: new LokiStore({
+        store: new FileStore({
           path: path.resolve(__dirname, '..', 'tmp', 'sessions')
         }),
         saveUninitialized: true
